25d3949daec1a898c786cc1f4824abab3e6000f9
Carian, Algen <201911008@gordoncollege.edu.ph>
2022-03-22 18:25:32 +0800

    midterm-exam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MidtermExamComponent } from './midterm-exam/midterm-exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MidtermExamComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/midterm-exam/midterm-exam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-midterm-exam',
+  templateUrl: './midterm-exam.component.html',
+  styleUrls: ['./midterm-exam.component.css']
+})
+export class MidtermExamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
